24252a045a74e02f93b038c4f0b3d29bcca12e84
Jacob Mobin <jacobamobin@gmail.com>
2025-01-02 19:13:00 -0500

    Initial Commit

---
+++ LearningPathwaysFrontendIOS/LearningPathwaysFrontendIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningPathwaysFrontendIOSApp.swift
+//  LearningPathwaysFrontendIOS
+//
+//  Created by Jacob Mobin on 2025-01-02.
+//
+
+import SwiftUI
+
+@main
+struct LearningPathwaysFrontendIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
